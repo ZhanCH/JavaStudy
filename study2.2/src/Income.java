9d851448c09a2a6eb06103b9ea2d80bf3cb1d8ee
ZhanCH <zch_178@163.com>
2022-08-20 16:07:12 +0800

    object-oriented 2.2

--- study2.2/src/Income.java
+++ study2.2/src/Income.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Income {
+abstract class finalIncome {
+    protected double income;
+
+    public finalIncome(double income) {
+        this.income = income;
+    }
+
+    public abstract double getTax();
+}
+
+class Income extends finalIncome {
+
+    public Income(double income) {
+        super(income);
+    }
+
+    @Override
+    public double getTax() {
+        return income * 0.8;
+    }
+}
+
+class newIncome extends finalIncome {
+    public newIncome(double income) {
+        super(income);
+    }
+
+    @Override
+    public double getTax() {
+        return income * 0.9;
+    }
 }
--- study2.2/src/Main.java
+++ study2.2/src/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+//多态
+//面向抽象编程
+public class Main {
+    public static void main(String[] args) {
+        finalIncome[] Tax = {
+                new Income(5000),
+                new newIncome(9000),
+        };
+        double total = 0;
+        for (finalIncome n : Tax)
+            total = total + n.getTax();
+        System.out.println(total);
+    }
 }
